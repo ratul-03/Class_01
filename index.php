0a66f514a70e5cb21bfd167e5b69b8f5b16c9410
Ratul_Always <168064170+ratul-03@users.noreply.github.com>
2025-08-04 18:02:14 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// If I want to show the data I have to use echo.
+echo "Hello World! ";
+
+/*
+ * I have to give $ sign to create a variable.
+*/
+$name = "John Doe";
+$price = "100 tk";
+$address = "Uttara House Building Number 6 Sector 8 Road Uttara Dhaka 1230, VCF2+2HX, Dhaka 1230";
+$number1 = 10;
+$number2 = 20;
+
+// I can use the variable inside the echo statement.
+echo "My name is $name. I live in $address. The price is $price. ";
+?>
